950e2622d509d10c1a69800c503b3d8fadc68d84
subhash <subhash.tiwari@skillbhoomi.com>
2024-01-24 21:17:55 +0530

    24 jan second commit

--- src/pages/chatmain/UserProfile.jsx
+++ src/pages/chatmain/UserProfile.jsx
@@ -1,15 +1,16 @@
-import React, { useState } from 'react';
-import './UserProfile.css';
-import { Link } from 'react-router-dom';
-import Plans from './Plans';
-import PersonalInformation from './PersonalInformation';
-import ChangePassword from './ChangePassword';
-import GeneralButton from '../../components/common/buttons/GeneralButton';  // Update the path accordingly
-import frame1 from '../../asset/Frame 1.png';
-import lefticon from '../../asset/arrow-left.png'
+import React, { useState } from "react";
+import "./UserProfile.css";
+import { Link } from "react-router-dom";
+import Plans from "./Plans";
+import PersonalInformation from "./PersonalInformation";
+import ChangePassword from "./ChangePassword";
+import GeneralButton from "../../components/common/buttons/GeneralButton"; // Update the path accordingly
+import TabNavigation from "../../components/common/tabNavigation/tabNaviagation"; // Update the path accordingly
+import frame1 from "../../asset/Frame 1.png";
+import lefticon from "../../asset/arrow-left.png";
 
 const UserProfile = () => {
-  const [selectedTab, setSelectedTab] = useState('personalinformation');
+  const [selectedTab, setSelectedTab] = useState("personalinformation");
 
   const handleTabChange = (tab) => {
     if (tab !== selectedTab) {
@@ -23,7 +24,7 @@ const UserProfile = () => {
         <div className="userprofile-sidebar">
           <div className="sidebar-amchat">
             <h2>Am-Chat</h2>
-            <div className='startchat-button'>
+            <div className="startchat-button">
               <GeneralButton
                 name="Start Chat"
                 type="primary"
@@ -37,13 +38,16 @@ const UserProfile = () => {
               />
             </div>
             <div className="back-link">
-              <Link to="/AmchatMainUser"><img className='left-back-icon' src={lefticon} alt="" />Back to chat</Link>
+              <Link to="/AmchatMainUser">
+                <img className="left-back-icon" src={lefticon} alt="" />
+                Back to chat
+              </Link>
             </div>
           </div>
         </div>
         <div className="userprofile-pofilecontainer">
           <div className="userprofile-header">
-            <div className='userprofile-profile'>
+            <div className="userprofile-profile">
               <h2>Profile</h2>
             </div>
             <div className="userprofile-account">
@@ -51,44 +55,27 @@ const UserProfile = () => {
                 <img src="" alt="" />
               </div>
               <div className="userprofile-account-name">
-                <div className='profile-account-img'></div>
+                <div className="profile-account-img"></div>
                 <h2>Clayton Santos</h2>
               </div>
             </div>
           </div>
-          <div className="info-bar">
-          <div className="info-bar-content">
-            <Link
-              className={`barinfo-personalinfo ${selectedTab === 'personalinformation' ? 'active-link' : ''}`}
-              onClick={() => handleTabChange('personalinformation')}>
-                Personal Information
-            </Link>
-            <Link
-              to="#"
-              className={`barinfo-changepassword ${selectedTab === 'changepassword' ? 'active-link' : ''}`}
-              onClick={() => handleTabChange('changepassword')}
-            >
-              Change Password
-            </Link>
-            <Link
-              to="#"
-              className={`barinfo-plans ${selectedTab === 'plans' ? 'active-link' : ''}`}
-              onClick={() => handleTabChange('plans')}
-            >
-              Plans
-            </Link>
+          <TabNavigation
+            selectedTab={selectedTab}
+            handleTabChange={handleTabChange}
+          />
+          <div>
+            {selectedTab === "personalinformation" && <PersonalInformation />}
+            {selectedTab === "changepassword" && <ChangePassword />}
+            {selectedTab === "plans" && <Plans />}
           </div>
-          {selectedTab === 'personalinformation' && <PersonalInformation />}
-            {selectedTab === 'changepassword' && <ChangePassword />}
-            {selectedTab === 'plans' && <Plans />}
-        </div>
-        </div>
-        </div>
-        <div className='userprofile-footer'>
-          <p>@2024. All rights reserved by AM Chatbot.</p>
         </div>
       </div>
-      );
+      <div className="userprofile-footer">
+        <p>@2024. All rights reserved by AM Chatbot.</p>
+      </div>
+    </div>
+  );
 };
 
-      export default UserProfile;
+export default UserProfile;
