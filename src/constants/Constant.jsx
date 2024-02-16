50d20ad0eb719827ebd77dc932151863745d10d1
RajeevPatel <rajeev.patel@areteminds.com>
2024-02-16 19:27:40 +0530

    todays commit

--- src/constants/Constant.jsx
+++ src/constants/Constant.jsx
@@ -1,8 +1,8 @@
-export const BASE_API_URL = "http://44.211.222.146:8080";
+export const BASE_API_URL = "https://amchatdev.areteminds.com/api/v1/iam";
 export const BASE_USER_IMAGE_URL = "https://medicalpublic.s3.amazonaws.com/";
 
 export const SIGNUP_ENDPOINT = "/user/signup";
-export const SIGNIN_ENDPOINT = "/users/login";
+export const SIGNIN_ENDPOINT = "/login";
 export const RECOVERY_PASSWORD_ENDPOINT = "/user/verification";
 export const CONTACT_US_ENDPOINT = "/user/contactUs";
 export const DOCUMENT_ENDPOINT = "/document";
