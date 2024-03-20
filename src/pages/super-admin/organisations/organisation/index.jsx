7754020fb786a3a417091fef1511ad7c63f5c3b7
Ashish <ashish.jha@areteminds.com>
2024-03-20 12:53:57 +0530

    made the privacy policy page and sign in page responsive

--- src/pages/super-admin/organisations/organisation/index.jsx
+++ src/pages/super-admin/organisations/organisation/index.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Layout from '../../../../Layout'
 function Orgsnisation() {
   return (
-    <Layout>
+    <Layout componentName="Add Organisations">
         Ravi tej
     </Layout>
   )
