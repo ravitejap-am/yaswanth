f69cffc518743b7aeefbc73b1316d14e4a4213f9
CodeWithPassion000 <shivakumararadyasm5@gmail.com>
2024-03-22 11:23:07 +0530

    bug fixes

--- src/index.js
+++ src/index.js
@@ -10,7 +10,35 @@ import { PersistGate } from 'redux-persist/integration/react';
 import { persistor, store } from '../src/store/config';
 import { MessageProvider } from './contexts/provider/MessageProvider';
 import { ChatProvider } from './contexts/provider/ChatContext';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
+const theme = createTheme({
+  typography: {
+    fontFamily: [
+      'Montserrat', // Add your preferred font family here
+      'Arial', // Backup font in case the preferred font is not available
+      'sans-serif',
+    ].join(','),
+    h1: {
+      fontWeight: 600, // Bold
+    },
+    h2: {
+      fontWeight: 600, // Bold
+    },
+    h3: {
+      fontWeight: 600, // Bold
+    },
+    h4: {
+      fontWeight: 600, // Bold
+    },
+    h5: {
+      fontWeight: 500, // Semi-bold
+    },
+    h6: {
+      fontWeight: 500, // Semi-bold
+    },
+  },
+});
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
@@ -18,7 +46,9 @@ root.render(
       <PersistGate loading={null} persistor={persistor}>
         <MessageProvider>
           <ChatProvider>
-            <App />
+            <ThemeProvider theme={theme}>
+              <App />
+            </ThemeProvider>
           </ChatProvider>
         </MessageProvider>
       </PersistGate>
