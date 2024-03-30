e07cc412de999ecc9b1868ed4562415a7e990296
Zaid Ansari <zaid.ansari@areteminds.com>
2024-03-30 14:34:11 +0530

    Graph changes and bug fixes

--- src/components/common/barChart/Bar.jsx
+++ src/components/common/barChart/Bar.jsx
@@ -2,17 +2,22 @@ import React, { useEffect } from "react";
 import * as echarts from "echarts";
 import { Box } from "@mui/material";
 
-const Bar = () => {
+const Bar = ({ dateList }) => {
   useEffect(() => {
     const chartDom = document.getElementById("bar");
     const myChart = echarts.init(chartDom);
-
+    console.log("dateList ===> ", dateList);
+    const xAxisData = Object.keys(dateList);
+    console.log(
+      "dateList ===> ",
+      xAxisData.map((date) => dateList[date].chat_count)
+    );
     const option = {
-      title: {
-        text: "Chats and Sessions",
-        // subtext: "Fake Data",
-        left: "left",
-      },
+      // title: {
+      //   text: "Chats and Sessions",
+      //   subtext: "Fake Data",
+      //   left: "left",
+      // },
       tooltip: {
         trigger: "axis",
       },
@@ -33,20 +38,7 @@ const Bar = () => {
       xAxis: [
         {
           type: "category",
-          data: [
-            "Jan",
-            "Feb",
-            "Mar",
-            "Apr",
-            "May",
-            "Jun",
-            "Jul",
-            "Aug",
-            "Sep",
-            "Oct",
-            "Nov",
-            "Dec",
-          ],
+          data: xAxisData,
         },
       ],
       yAxis: [
@@ -58,34 +50,30 @@ const Bar = () => {
         {
           name: "Chat Counts",
           type: "bar",
-          data: [
-            2.0, 4.9, 7.0, 23.2, 25.6, 76.7, 135.6, 162.2, 32.6, 20.0, 6.4, 3.3,
-          ],
-          markPoint: {
-            data: [
-              { type: "max", name: "Max" },
-              { type: "min", name: "Min" },
-            ],
-          },
-          markLine: {
-            data: [{ type: "average", name: "Avg" }],
-          },
+          data: xAxisData.map((date) => dateList[date].chat_count),
+          // markPoint: {
+          //   data: [
+          //     { type: "max", name: "Max" },
+          //     { type: "min", name: "Min" },
+          //   ],
+          // },
+          // markLine: {
+          //   data: [{ type: "average", name: "Avg" }],
+          // },
         },
         {
           name: "Session Counts",
           type: "bar",
-          data: [
-            2.6, 5.9, 9.0, 26.4, 28.7, 70.7, 175.6, 182.2, 48.7, 18.8, 6.0, 2.3,
-          ],
-          markPoint: {
-            data: [
-              { name: "Max", value: 182.2, xAxis: 7, yAxis: 183 },
-              { name: "Min", value: 2.3, xAxis: 11, yAxis: 3 },
-            ],
-          },
-          markLine: {
-            data: [{ type: "average", name: "Avg" }],
-          },
+          data: xAxisData.map((date) => dateList[date].session_count),
+          // markPoint: {
+          //   data: [
+          //     { name: "Max", value: 182.2, xAxis: 7, yAxis: 183 },
+          //     { name: "Min", value: 2.3, xAxis: 11, yAxis: 3 },
+          //   ],
+          // },
+          // markLine: {
+          //   data: [{ type: "average", name: "Avg" }],
+          // },
         },
       ],
     };
@@ -95,7 +83,7 @@ const Bar = () => {
     return () => {
       myChart.dispose();
     };
-  }, []);
+  }, [dateList]);
 
   return <Box id="bar" />;
 };
