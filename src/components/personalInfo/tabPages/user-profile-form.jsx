d271b25a56261a49bf02c1a8604ec01b1416dc41
ravi <ravi.teja@areteminds.com>
2024-04-03 10:25:37 +0530

    resolved cancel button issue

--- src/components/personalInfo/tabPages/user-profile-form.jsx
+++ src/components/personalInfo/tabPages/user-profile-form.jsx
@@ -139,34 +139,20 @@ function UserProfileForm({ formData, setFormData, submitHandler }) {
             </div>
           </div>
         </div>
-        {/* <div className="form-row">
-    <div className="form-group">
-      <label htmlFor="status">Status:</label>
-      <input
-        className="inputstyle"
-        type="text"
-        id="status"
-        name="status"
-        value={formData.status}
-        onChange={handleChange}
-        disabled
-        style={{ backgroundColor: '#CBD5E1' }}
-      />
-      {errors.status && <span className="error">{errors.status}</span>}
-    </div>
-  </div> */}
         <div
           className="button-container"
           style={{ justifyContent: isMobile ? "center" : "flex-end"}}
         >
           <Button
-            onClick={cancelHandler}
-            className="cancelButtonStyle"
+            htmlType="cancel"
+            className="buttonStyle"
+            style={{backgroundColor: 'white', color: 'black'}}
           >
-            <Typography variant="button"> Cancel </Typography>
+            <Typography variant="button" display="block">
+              Cancel
+            </Typography>
           </Button>
           <Button
-            type="primary"
             htmlType="submit"
             className="buttonStyle"
           >
