f367ad18258f801838773dd43e4b5a5e6e52cd57
CodeWithPassion000 <shivakumararadyasm5@gmail.com>
2024-04-17 15:51:05 +0530

    personal information completd

--- src/components/personalInfo/page.jsx
+++ src/components/personalInfo/page.jsx
@@ -19,17 +19,16 @@ const PersonalInfo = () => {
   const [selectedTab, setSelectedTab] = useState('1');
   const [userRoles, setUserRoles] = useState([]);
 
-
   useEffect(() => {
     const rolesString = localStorage.getItem('userRole');
     if (rolesString) {
-      const roles = rolesString.split(',').map(role => role.trim());
+      const roles = rolesString.split(',').map((role) => role.trim());
       setUserRoles(roles);
     }
   }, []);
-  
 
-  const isAllowedToSeePlans = userRoles.includes('USER') || userRoles.includes('ORG_ADMIN');
+  const isAllowedToSeePlans =
+    userRoles.includes('USER') || userRoles.includes('ORG_ADMIN');
 
   const handleTabChange = (event, newValue) => {
     setSelectedTab(newValue);
@@ -40,35 +39,16 @@ const PersonalInfo = () => {
   };
 
   return (
-    <Box sx={{ width: '97%', typography: 'body1', paddingTop: '10px', padding: '1rem', height: '100%'}}>
+    <Box
+      sx={{ width: '97%', paddingTop: '10px', padding: '1rem', height: '100%' }}
+    >
       <TabContext value={selectedTab}>
-        {/* {isMobile ? (
-          <Box sx={{ marginBottom: 2 }}>
-            <Select
-              value={selectedTab}
-              onChange={handleSelectChange}
-              fullWidth
-              sx={{ backgroundColor: 'white', borderRadius: 4, height: 50}}
-            >
-              <MenuItem value="1">
-                <Typography fontWeight="bold">Personal Information</Typography>
-              </MenuItem>
-              <MenuItem value="2">
-                <Typography fontWeight="bold">Change Password</Typography>
-              </MenuItem>
-              <MenuItem value="3">
-                <Typography fontWeight="bold">Plans</Typography>
-              </MenuItem>
-            </Select>
-          </Box>
-        ) : ( */}
-        <Box sx={{ borderBottom: 1, borderColor: 'divider'}}>
+        <Box>
           <TabList
             onChange={handleTabChange}
             aria-label="Personal Info Tabs"
             variant="scrollable"
             scrollButtons="auto"
-            sx={{ backgroundColor: 'white', borderRadius: 4 }}
           >
             <Tab
               label={
@@ -81,21 +61,29 @@ const PersonalInfo = () => {
               value="2"
             />
             {isAllowedToSeePlans && (
-              <Tab label={<Typography fontWeight="bold">Usage/Subscription</Typography>} value="3" />
+              <Tab
+                label={
+                  <Typography fontWeight="bold">Usage/Subscription</Typography>
+                }
+                value="3"
+              />
             )}
           </TabList>
         </Box>
         {/* )} */}
-        <Box sx={{height: '70%'}}>
-        <TabPanel value="1" style={{ padding: '0px', height: '100%' }}>
-          <Information />
-        </TabPanel>
-        <TabPanel value="2" style={{height: '70%',padding: '30px 0px 0px 0px'}}>
-          <ChangePassword />
-        </TabPanel>
-        <TabPanel value="3">
-          <PersonalPlans />
-        </TabPanel>
+        <Box sx={{ height: '70%' }}>
+          <TabPanel value="1" style={{ padding: '0px', height: '100%' }}>
+            <Information />
+          </TabPanel>
+          <TabPanel
+            value="2"
+            style={{ height: '70%', padding: '30px 0px 0px 0px' }}
+          >
+            <ChangePassword />
+          </TabPanel>
+          <TabPanel value="3">
+            <PersonalPlans />
+          </TabPanel>
         </Box>
       </TabContext>
     </Box>
