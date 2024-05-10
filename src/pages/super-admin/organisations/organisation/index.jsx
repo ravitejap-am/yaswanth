936957c3f2ebf3d3fd4ee80ed4641f3aac94da70
RajeevPatel <rajeevpatel8985@gmail.com>
2024-05-10 11:46:56 +0530

    file name issue

--- src/pages/super-admin/organisations/organisation/index.jsx
+++ src/pages/super-admin/organisations/organisation/index.jsx
@@ -37,7 +37,7 @@ import TabContext from "@mui/lab/TabContext";
 import TabList from "@mui/lab/TabList";
 import TabPanel from "@mui/lab/TabPanel";
 import { State } from "country-state-city";
-import OrganisationLayout from "./OrganisationLayout";
+import OrganisationLayout from "./organisationLayout";
 
 function Organisation() {
   const isAndroid = /Android/.test(navigator.userAgent);
